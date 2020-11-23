7bac5f09def03c3aeb7bc630c7d681c9d173173f
Malte Peters <malte@networkfreaks.de>
2020-11-23 12:15:11 +0100

    Added test project

---
+++ Test-Magick-Docker3-1/Controllers/ImageTestController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test_Magick_Docker3_1.Controllers
+{
+    [Route("ImageTest")]
+    public class ImageTestController : Controller
+    {
+        private string testfile = null;
+        private string nativeidr = null;
+
+        public ImageTestController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.testfile = System.IO.Path.Combine(hostingEnvironment.ContentRootPath, "Sources", "test.jpg");
+            this.nativeidr = System.IO.Path.Combine(hostingEnvironment.ContentRootPath, @"bin\Debug\netcoreapp3.1\runtimes\win-x64\native");
+             
+        }
+
+        [HttpGet("Test1")]
+        public IActionResult Test1()
+        {
+            ImageMagick.MagickImage img = new ImageMagick.MagickImage(this.testfile);
+
+            img.Scale(100, 100);
+
+            byte[] result = img.ToByteArray();
+
+            return File(result, "image/jpeg");
+        }
+
+
+        [HttpGet("Test2")]
+        public IActionResult Test2()
+        {
+            // This time set native dir
+            ImageMagick.MagickNET.SetNativeLibraryDirectory(this.nativeidr);
+            ImageMagick.MagickImage img = new ImageMagick.MagickImage(this.testfile);
+
+            img.Scale(100, 100);
+
+            byte[] result = img.ToByteArray();
+
+            return File(result, "image/jpeg");
+        }
+
+
+    }
+}
---
+++ Test-Magick-Docker3-1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test_Magick_Docker3_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
